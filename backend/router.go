374be6f09de88118ac6f18e98cf211b9f12aeea3
R. Abid Hermawan <97014247+rabidhermawan@users.noreply.github.com>
2025-11-28 12:34:30 +0700

    BREAKING CHANGE : requires auth for event creation, update and deletion

--- backend/router.go
+++ backend/router.go
@@ -16,21 +16,28 @@ func init() {
 func main() {
 	r := gin.Default()
 
-	// createEvent()
-
+	// --- For guest & user ---
 	// Events
 	r.GET("/events", controllers.GetEventAll)
 	r.GET("/events/:id", controllers.GetEventFromID)
-	r.POST("/events/create", controllers.CreateEvent)
-
-	r.PATCH("/events/update/:id", controllers.UpdateEventByID)
-	r.DELETE("/events/update/:id", controllers.DeleteEventByIDSoft)
-	r.POST("/events/update/images/:id", controllers.UploadEventImage)
 
 	// Users
 	r.POST("/signup", controllers.Signup)
 	r.GET("/login", controllers.Signin)
-	r.GET("/ping", middleware.RequireAuth, controllers.TestPing)
+
+	// --- For user only ---
+	authorized := r.Group("/")
+	authorized.Use(middleware.RequireAuth)
+	{
+		// Events
+		authorized.POST("/events/create", controllers.CreateEvent)
+		authorized.PATCH("/events/update/:id", controllers.UpdateEventByID)
+		authorized.POST("/events/update/images/:id", controllers.UploadEventImage)
+		authorized.DELETE("/events/update/:id", controllers.DeleteEventByIDSoft)
+
+		// Testing
+		authorized.GET("/ping", controllers.TestPing)
+	}
 
 	r.Run("127.0.0.1:8080")
 }
